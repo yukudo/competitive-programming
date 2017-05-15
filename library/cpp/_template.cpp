76f2ea1d2ccc16eaba63b2ba0fa0234058df47d6
ykudo <kudoyu@team-lab.com>
2017-05-15 18:24:23 +0900

    b

---
+++ library/cpp/_template.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define REP(i,n) for(int i=0,_n=(int)(n);i<_n;i++)
+template<class T>bool chmin(T&a,const T&b){if(a<=b)return false;a=b;return true;}
+template<class T>bool chmax(T&a,const T&b){if(a>=b)return false;a=b;return true;}
+
+int nextInt() { int x; scanf("%d", &x); return x;}
+
+int main2() {
+  int n = nextInt();
+  ll ans = 0;
+  cout << ans << endl;
+  return 0;
+}
+
+int main() {
+  for (;!cin.eof();cin>>ws) main2();
+  return 0;
+}
+
