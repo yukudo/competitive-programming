6796a84ba5fc783bafa1de79a9c8a71ca5f76ed8
yukudo <kudo.y.aa@gmail.com>
2017-05-16 17:30:58 +0900

    m

--- library/cpp/mod.cpp
+++ library/cpp/mod.cpp
@@ -7,3 +7,17 @@ ll mod_pow(ll a, ll b, ll p) {
   }
   return res;
 }
+
+ll mod_inv(ll a, ll p) {
+  return mod_pow(a % p, p - 2, p);
+}
+
+// ## 逆元の列挙 O(N)
+// これで 1 から N までの逆元が O(N) でもとまります。i * (MOD/i) = MOD - MOD%i を変形すると出てきます。
+// https://twitter.com/rng_58/status/312405554339405824
+// https://twitter.com/rng_58/status/312413291647492096
+{
+  ll inv[N];
+  inv[1] = 1;
+  for (int i = 2; i < N; i++) inv[i] = inv[(int) (MOD % i)] * (MOD - MOD / i) % MOD;
+}
