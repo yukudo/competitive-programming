86b41d627249a4f9681c29c659478d7b18bb25de
yukudo <kudo.y.aa@gmail.com>
2018-03-30 23:58:06 +0900

    a

--- library/cpp/normalize.cpp
+++ library/cpp/normalize.cpp
@@ -1,16 +1,14 @@
 // 座標圧縮
-vector<int> normalize(vector<int> v) {
+vector<int> normalize(const vector<ll> v) {
   const int N = v.size();
-  vector<pair<int,int> > t(N);
-  REP(i, N) {
-    t[i].first = v[i];
-    t[i].second = i;
-  }
+  vector<int> res(N);
+  vector<pair<ll,int> > t(N);
+  REP(i, N) t[i] = { v[i], i };
   sort(ALL(t));
   int r = 0;
   REP(i, N) {
     r += (i > 0 && t[i - 1].first != t[i].first);
-    v[t[i].second] = r;
+    res[t[i].second] = r;
   }
-  return v;
+  return res;
 }
