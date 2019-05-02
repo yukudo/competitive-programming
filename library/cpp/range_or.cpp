d75ce72077589794da86e287a9e983f59fbb4cf0
yukudo <kudo.y.aa@gmail.com>
2019-05-03 03:24:45 +0900

    update

--- library/cpp/range_or.cpp
+++ library/cpp/range_or.cpp
@@ -3,12 +3,14 @@
 // i を固定して j を動かすとき、取りうる値は高々[要素のビット数]種類である。
 // 理由は、立っているビットの数が変化するのがせいぜいビット数回なので。
 // 各 i について、それぞれの値を取る最初の位置を求める。
+// さらに、どの値が何回出現するかを map に持つことだってできる。
 
 const int MAX_N = 212345;
 int N;
 ll A[MAX_N];
 int next1[MAX_N][64]; // next1[i][k] := k ビット目について位置 i より後ろで最も早く 1 が出てくる場所
 vector<pair<ll,int>> B[MAX_N]; // B[i] := 区間[i, *]についてのorについて、値が変わる場所とその値
+map<ll, ll> occur; // occur[val] := 値valが出現する回数
 
 void build() {
   REP(k, 64) {
@@ -26,6 +28,14 @@ void build() {
     for (int j : js) if (j < N) {
       val |= A[j];
       if (list.empty() || list.back().first != val) list.push_back({val,j});
+    }
+  }
+  REP(i, N) {
+    REP(j, B[i].size()) {
+      ll val = B[i][j].first;
+      int beg = B[i][j].second;
+      int end = j+1 < (int)B[i].size() ? B[i][j+1].second : N;
+      occur[val] += end - beg;
     }
   }
 }
