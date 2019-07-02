65a6d6f51e9fa07fb21857767b0874b87845ebec
yukudo <kudo.y.aa@gmail.com>
2019-07-02 14:53:06 +0900

    Update normalize.cpp

--- library/cpp/normalize.cpp
+++ library/cpp/normalize.cpp
@@ -1,14 +1,18 @@
+
 // 座標圧縮
+// vector<ll> rev; // 圧縮後の値から元の値への写像
 vector<int> normalize(const vector<ll> v) {
   const int N = v.size();
   vector<int> res(N);
   vector<pair<ll,int> > t(N);
+  // rev = vector<ll>(N);
   REP(i, N) t[i] = { v[i], i };
   sort(ALL(t));
   int r = 0;
   REP(i, N) {
     r += (i > 0 && t[i - 1].first != t[i].first);
     res[t[i].second] = r;
+    // rev[r] = t[i].first;
   }
   return res;
 }
