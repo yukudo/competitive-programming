b6860c35b1dcff62371766ae59cfa3ff92fbca56
yukudo <kudo.y.aa@gmail.com>
2019-10-22 03:20:24 +0900

    Update binary_indexed_tree.cpp

--- library/cpp/segtree/binary_indexed_tree.cpp
+++ library/cpp/segtree/binary_indexed_tree.cpp
@@ -12,27 +12,28 @@ int highestOneBit(unsigned int i) {
 // 点に加算、範囲の和
 // 内部的には 1-based であるが、利用するときには 0-based な実装
 struct BIT {
+  using Val = int;
   int n;
-  vector<ll> tree;
-  BIT(int n):n(n),tree(n+1) {} 
-  void fill(ll val) {
+  vector<Val> tree;
+  BIT(int n):n(n),tree(n+1) {}
+  void fill(Val val) {
     REP(i, n+1) tree[i] = (i&-i) * val;
   }
-  void add(int idx, ll val) {
+  void add(int idx, Val val) {
     for (int x=idx+1; x<=n; x+=x&-x) tree[x] += val;
   }
   // sum[0, idx]
-  ll range(int idx) {
-    ll sum=0;
+  Val range(int idx) {
+    Val sum=0;
     for (int x=idx+1; x>0; x-=x&-x) sum += tree[x];
     return sum;
   }
   // sum[a, b]
-  ll range(int a, int b) {
+  Val range(int a, int b) {
     return range(b) - range(a - 1);
   }
   // sum [0, i] が k より大きい最小の i を求める
-  int get(ll k) {
+  int get(Val k) {
     int p = highestOneBit(n);
     for (int q = p; q > 0; q >>= 1, p |= q) {
       if (p >= n || k < tree[p]) p ^= q;
