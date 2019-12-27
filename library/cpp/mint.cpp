a6d142ce90449b1e02e11275a1a12a9767375698
yukudo <kudo.y.aa@gmail.com>
2019-12-28 03:24:26 +0900

    Create mint.cpp

---
+++ library/cpp/mint.cpp
@@ -0,0 +1,24 @@
+struct mint {
+  ll x;
+  mint(ll x=0):x((x%MOD+MOD)%MOD){}
+  mint& operator+=(const mint a) {if ((x += a.x) >= MOD) x -= MOD;return *this;}
+  mint& operator-=(const mint a) {if ((x += MOD-a.x) >= MOD) x -= MOD;return *this;}
+  mint& operator*=(const mint a) {(x *= a.x) %= MOD;return *this;}
+  mint operator+(const mint a) const {mint res(*this);return res+=a;}
+  mint operator-(const mint a) const {mint res(*this);return res-=a;}
+  mint operator*(const mint a) const {mint res(*this);return res*=a;}
+  mint pow(ll b) const {
+  	mint res = 1;
+    mint a = *this;
+    while (b) {
+    	if (b & 1) res *= a;
+    	a *= a;
+    	b >>= 1;
+    }
+    return res;
+  }
+  // for prime MOD
+  mint inv() const {return pow(MOD-2);}
+  mint& operator/=(const mint a) {return (*this) *= a.inv();}
+  mint operator/(const mint a) const {mint res(*this);return res/=a;}
+};
