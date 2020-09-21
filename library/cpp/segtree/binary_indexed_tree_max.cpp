2a39f0eeb924e7cdb83c7da6f5801a472c4f3a7d
yukudo <kudo.y.aa@gmail.com>
2020-09-21 19:00:09 +0900

    Update binary_indexed_tree_max.cpp

--- library/cpp/segtree/binary_indexed_tree_max.cpp
+++ library/cpp/segtree/binary_indexed_tree_max.cpp
@@ -3,22 +3,23 @@
 // - 範囲がどちら向きかはコメントアウトしてるところを切り替える
 // - max を min に変えると BITMIN になる
 // - 初期値は 0/INF/-INF を問題によって決める
+using Val = ll;
+Val default_value = 0;
 struct BITMAX {
   int n;
-  int vs[MAX_N + 1];
-  BITMAX() {
-    this->n = MAX_N;
+  vector<Val> vs;
+  BITMAX(int n) : n(n), vs(n+1) {
     clear();
   }
   void clear() {
-    REP(i, n+1) vs[i] = 0; // default value
+    REP(i, n+1) vs[i] = default_value;
   }
-  void update(int idx, int val) {
+  void update(int idx, Val val) {
     for (int x = idx + 1; x > 0; x -= x & -x) vs[x] = max(vs[x], val); // [0, idx] の範囲の最大値を val で更新しようと試みる
     // for (int x = idx + 1; x <= n; x += x & -x) vs[x] = max(vs[x], val); // [idx, ∞] の範囲の最大値を val で更新しようと試みる
   }
-  int get(int idx) {
-    int val = 0; // default value
+  Val get(int idx) {
+    Val val = default_value;
     for (int x = idx + 1; x <= n; x += x & -x)  val = max(val, vs[x]); // [0, idx] の範囲の最大値を取得
     // for (int x = idx + 1; x > 0; x -= x & -x) val = max(val, vs[x]); // [idx, ∞] の範囲の最大値を取得
     return val;
