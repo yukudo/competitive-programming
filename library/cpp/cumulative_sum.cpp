3a9eb42f835fe0fb07323ccc074060ef4db6bb01
yukudo <kudo.y.aa@gmail.com>
2021-05-20 15:44:03 +0900

    Create cumulative_sum.cpp

---
+++ library/cpp/cumulative_sum.cpp
@@ -0,0 +1,28 @@
+template<class T>
+struct CumulativeSum {
+  int R, C;
+  vector<vector<T>> acc;
+
+  CumulativeSum(int R, int C) {
+    this->R = R;
+    this->C = C;
+    acc.assign(R+1, vector<T>(C+1));
+  }
+
+  void build() {
+    REP(i, R) REP(j, C) {
+      acc[i+1][j+1] += acc[i+1][j] + acc[i][j+1] - acc[i][j];
+    }
+  }
+
+  // for (int r = r0; r < r1; r++) for (int c = c0; c < c1; c++) res += A[r][c];
+  T get(int r0, int c0, int r1, int c1) const {
+    return acc[r1][c1] - acc[r0][c1] - acc[r1][c0] + acc[r0][c0];
+  }
+
+  // A[r][c] += v;
+  void add(int r, int c, T v) {
+    if (!(0 <= r && r < R && 0 <= c && c < C)) return;
+    acc[r+1][c+1] += v;
+  }
+};
