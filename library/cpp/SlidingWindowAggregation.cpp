c898aa35722026cd36eecff8d38838cdd3e0232a
yukudo <kudo.y.aa@gmail.com>
2023-11-03 04:48:51 +0900

    Update SlidingWindowAggregation.cpp

--- library/cpp/SlidingWindowAggregation.cpp
+++ library/cpp/SlidingWindowAggregation.cpp
@@ -1,9 +1,16 @@
 //
 // deque の4操作に加えて prod_all() が行えるデータ構造。特殊ケースとして slide_min を作れる。
 // 
+const ll MOD = 998244353;
+struct F { int a, b; };
 
-int op(int a, int b) { return min(a, b); }
-int e() { return (int)(1e9); }
+F op(F f1, F f2) { 
+  return (F){ 
+    (int)((ll)f2.a * f1.a % MOD),
+    (int)(((ll)f2.a * f1.b + f2.b) % MOD),
+  };
+}
+F e() { return (F){ 1, 0 }; }
 
 template <class S, auto op, auto e>
 struct SlidingWindowAggregation {
@@ -11,9 +18,13 @@ struct SlidingWindowAggregation {
   struct FoldableStack {
     vector<S> vals, prods;
     bool empty() const { return vals.empty(); }
-    void push(S val) {
+    void push_r(S val) {
       vals.push_back(val);
       prods.push_back(prods.empty() ? val : op(prods.back(), val));
+    }
+    void push_l(S val) {
+      vals.push_back(val);
+      prods.push_back(prods.empty() ? val : op(val, prods.back()));
     }
     void pop() { if (!vals.empty()) { vals.pop_back(); prods.pop_back(); } }
     S all_prod() const { return prods.empty() ? e() : prods.back(); }
@@ -39,8 +50,8 @@ struct SlidingWindowAggregation {
     if (!frontStack.empty()) return frontStack.bottom();
     return e();
   }
-  void push_back(S val) { backStack.push(val); }
-  void push_front(S val) { frontStack.push(val); }
+  void push_back(S val) { backStack.push_r(val); }
+  void push_front(S val) { frontStack.push_l(val); }
   void pop_back() {
     if (empty()) return;
     if (!backStack.empty()) { backStack.pop(); return; }
@@ -50,9 +61,8 @@ struct SlidingWindowAggregation {
       frontStack.pop();
     }
     int n = tmp.size();
-    for (int i = n/2 - 1; i >= 0; i--) frontStack.push(tmp[i]);
-    for (int i = n/2; i < n; i++) backStack.push(tmp[i]);
-    backStack.pop();
+    for (int i = n/2 - 1; i >= 0; i--) frontStack.push_l(tmp[i]);
+    for (int i = n/2; i + 1 < n; i++) backStack.push_r(tmp[i]);
   }
   void pop_front() {
     if (empty()) return;
@@ -63,12 +73,49 @@ struct SlidingWindowAggregation {
       backStack.pop();
     }
     int n = tmp.size();
-    for (int i = n/2 - 1; i >= 0; i--) backStack.push(tmp[i]);
-    for (int i = n/2; i < n; i++) frontStack.push(tmp[i]);
-    frontStack.pop();
+    for (int i = n/2 - 1; i >= 0; i--) backStack.push_r(tmp[i]);
+    for (int i = n/2; i + 1 < n; i++) frontStack.push_l(tmp[i]);
   }
   // 現在入っている要素すべての op() を計算します。空のときは e() を返します。
   S all_prod() const {
     return op(frontStack.all_prod(), backStack.all_prod());
   }
 };
+
+int main2() {
+  
+  // https://judge.yosupo.jp/submission/169790
+
+  SlidingWindowAggregation<F, op, e> deq;
+
+  int Q = nextLong();
+  while (Q--) {
+    int t = nextLong();
+    if (t == 0) {
+      int a = nextLong();
+      int b = nextLong();
+      deq.push_back(F{a, b});
+    }
+    if (t == 1) {
+      deq.pop_front();
+    }
+    if (t == 2) {
+      int x = nextLong();
+      F f = deq.all_prod();
+      int ans = ((ll)f.a * x + f.b) % MOD;
+      cout << ans << '\n';
+    }
+  }
+  return 0;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+#ifdef LOCAL
+  for (;!cin.eof();cin>>ws)
+#endif
+    main2();
+  return 0;
+}
